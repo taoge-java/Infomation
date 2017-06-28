10f629842af839703050c13aed1ad55f0760807c
zengjintao <1913188966@SC-201706212248>
2017-06-28 16:23:05 +0800

    。。

---
+++ src/main/java/com/information/model/slave/SlaveBaseModel.java
@@ -0,0 +1,12 @@
+package com.information.model.slave;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class SlaveBaseModel extends Model<SlaveBaseModel>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
