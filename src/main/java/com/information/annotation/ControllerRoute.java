d5891917a0400105f589b0436c61cebe63ebc559
zengjintao <Administrator@SC-201706212248>
2017-07-03 14:39:59 +0800

    。

---
+++ src/main/java/com/information/annotation/ControllerRoute.java
@@ -0,0 +1,28 @@
+package com.information.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ *   1.CONSTRUCTOR:用于描述构造器
+　　　　2.FIELD:用于描述域
+　　　　3.LOCAL_VARIABLE:用于描述局部变量
+　　　　4.METHOD:用于描述方法
+　　　　5.PACKAGE:用于描述包
+　　　　6.PARAMETER:用于描述参数
+　　　　7.TYPE:用于描述类、接口(包括注解类型) 或enum声明
+ */
+
+/**
+ * 路由注解
+ * @author zengjintao
+ * @version 1.0
+ * @create_at 2017年7月3日下午2:35:24
+ */
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ControllerRoute {
+
+	String url();
+}
