690cb0792ba55083af071ec25fb26849affa55b9
zengjintao <Administrator@SC-201706212248>
2017-07-27 09:21:04 +0800

    代码修改

--- src/main/java/com/information/model/primary/BaseModel.java
+++ src/main/java/com/information/model/primary/BaseModel.java
@@ -1,7 +1,12 @@
 package com.information.model.primary;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Map;
 
+import com.jfinal.plugin.activerecord.Config;
+import com.jfinal.plugin.activerecord.DbKit;
 import com.jfinal.plugin.activerecord.Model;
 
 import java.util.Map.Entry;
@@ -35,6 +40,45 @@ public class BaseModel<M extends Model<M>> extends Model<M>{
 				}
 			}
 		}
-		
+	}
+	
+	/**
+	 * model删除方法
+	 * @param sql
+	 * @param paras
+	 * @return
+	 */
+	public boolean delete(String sql,Object...paras){
+		Config config=DbKit.getConfig();
+		return execute(config,sql,paras);
+			
+	}
+	
+	/**
+	 * model update方法
+	 * @param sql
+	 * @param paras
+	 * @return
+	 */
+	public boolean update(String sql,Object...paras){
+		Config config=DbKit.getConfig();
+		return execute(config,sql,paras);
+	}
+	
+	private boolean execute(Config config,String sql,Object...paras){
+		Connection conn;
+		try {
+			conn = config.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			for(int i=0;i<paras.length;i++){
+				statement.setObject(i+1,paras[i]);
+			}
+			statement.executeUpdate();
+			config.close(conn);
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
