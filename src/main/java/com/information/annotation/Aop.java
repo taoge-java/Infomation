3046bfa3f0508aaedc7dffbf5aee225471ac1c7b
zengjintao <Administrator@SC-201706212248>
2017-08-31 18:49:14 +0800

    。

--- src/main/java/com/information/annotation/Aop.java
+++ src/main/java/com/information/annotation/Aop.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD,ElementType.TYPE})
+@Target({ElementType.TYPE})
 @Documented
 public @interface Aop {
 	String value() default "";
