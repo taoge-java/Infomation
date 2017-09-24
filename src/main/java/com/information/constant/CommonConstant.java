35e762b983349c65ea2816a3552100c2670ae62e
zengjintao <Administrator@SC-201706212248>
2017-09-24 13:42:48 +0800

    。

--- src/main/java/com/information/constant/CommonConstant.java
+++ src/main/java/com/information/constant/CommonConstant.java
@@ -4,19 +4,27 @@ package com.information.constant;
  * @author Administrator
  *
  */
-public class CommonConstant {
+public final class CommonConstant {
 	
 	/*用户session常量*/
 	public static final String  SESSION_ID_KEY="session_id_key";
-	/**每页显示数量**/
-	public static final int pageSize=10;
+	
     /**图形验证码*/
 	public static final String IMAGE_CODE="image_code";
-	 /**用户cookie*/
-	public static final String COOKIE_USERNAME="username_cookie";
 	
+	/**用户cookie*/
 	public static final String COOKIE_USER_ID="cookie_user_id";
-		
-	public static final String COOKIE_PASSWORD="password_cookie";
-		
+	
+	/**一分钟缓存**/
+	public static final String ONE_MINUE="one.minute";
+	
+	/*redis缓存常量*/	
+	public static final String HOSTCAHENAME="information";
+	
+	/*cookie sessionid常量*/
+	public static final String  COOKIE_SESSION_ID_KEY="JSESSIONID";
+	
+	public static final String DEFAULT_ENCODING = "UTF-8";
+	
+	public static String SESSIONID;
 }
